4f8ca9c77af4f94d8231aca97af30ad08698fdd5
naren4488 <narendrakajla77@gmail.com>
2026-02-16 07:34:20 +0530

    scroll to top position

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Layout } from "@/components/layout/Layout"
+import { ScrollToTop } from "@/components/ScrollToTop"
 import { Home } from "@/pages/Home"
 import { AboutUs } from "@/pages/AboutUs"
 import { Services } from "@/pages/Services"
@@ -8,6 +9,7 @@ import { Contact } from "@/pages/Contact"
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
